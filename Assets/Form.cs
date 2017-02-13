4fac635948a7f2dd9bcc52f81b561373cf13d612
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-02-14 00:13:08 +0900

    諸々追加

---
+++ Assets/Form.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+public class Form : MonoBehaviour {
+    [DllImport("user32.dll")]
+    private static extern void OpenFileDialog();
+
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+
+    void Start () {
+        OpenFileAndPlay();
+	}
+
+    public void OpenFileAndPlay()
+    {
+        string filePath = OpenFile();
+
+        if (filePath == "")
+        {
+            return;
+        }
+
+        string filename = System.IO.Path.GetFileName(filePath);
+        videoPlayer.url = "file://" + filePath;
+        videoPlayer.Play();
+    }
+
+    private string OpenFile()
+    {
+        OpenFileDialog dialog = new OpenFileDialog();
+        DialogResult result = dialog.ShowDialog();
+
+        return dialog.FileName;
+    }
+}
